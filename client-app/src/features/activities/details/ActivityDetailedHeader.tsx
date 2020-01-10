f4e2af628ed66e9e9c9454860ae052c33687a42c
MLordship <m.signorino@gmail.com>
2020-01-10 09:43:31 +0100

    End of section 15

--- client-app/src/features/activities/details/ActivityDetailedHeader.tsx
+++ client-app/src/features/activities/details/ActivityDetailedHeader.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Segment, Image, Item, Header, Button } from "semantic-ui-react";
 import { IActivity } from "../../../app/models/activity";
 import { observer } from "mobx-react-lite";
 import { Link } from "react-router-dom";
 import { format } from "date-fns";
+import { RootStoreContext } from "../../../app/stores/rootStore";
 
 const activityImageStyle = {
   filter: "brightness(30%)"
@@ -18,26 +19,18 @@ const activityImageTextStyle = {
   color: "white"
 };
 
-const ActivityDetailedHeader: React.FC<{ activity: IActivity }> = ({
-  activity
-}) => {
+const ActivityDetailedHeader: React.FC<{ activity: IActivity }> = ({ activity }) => {
+  const rootStore = useContext(RootStoreContext);
+  const { attendActivity, cancelAttendance, loading } = rootStore.activityStore;
   return (
     <Segment.Group>
       <Segment basic attached="top" style={{ padding: "0" }}>
-        <Image
-          src={`/assets/categoryImages/${activity.category}.jpg`}
-          fluid
-          style={activityImageStyle}
-        />
+        <Image src={`/assets/categoryImages/${activity.category}.jpg`} fluid style={activityImageStyle} />
         <Segment basic style={activityImageTextStyle}>
           <Item.Group>
             <Item>
               <Item.Content>
-                <Header
-                  size="huge"
-                  content={activity.title}
-                  style={{ color: "white" }}
-                />
+                <Header size="huge" content={activity.title} style={{ color: "white" }} />
                 <p>{format(activity.date, "eeee do MMMM")}</p>
                 <p>
                   Hosted by <strong>Bob</strong>
@@ -48,16 +41,20 @@ const ActivityDetailedHeader: React.FC<{ activity: IActivity }> = ({
         </Segment>
       </Segment>
       <Segment clearing attached="bottom">
-        <Button color="teal">Join Activity</Button>
-        <Button>Cancel attendance</Button>
-        <Button
-          as={Link}
-          to={`/manage/${activity.id}`}
-          color="orange"
-          floated="right"
-        >
-          Manage Event
-        </Button>
+        {activity.isHost ? (
+          <Button as={Link} to={`/manage/${activity.id}`} color="orange" floated="right">
+            Manage Event
+          </Button>
+        ) : activity.isGoing ? (
+          <Button loading={loading} onClick={cancelAttendance}>
+            {" "}
+            Cancel attendance
+          </Button>
+        ) : (
+          <Button loading={loading} onClick={attendActivity} color="teal">
+            Join Activity
+          </Button>
+        )}
       </Segment>
     </Segment.Group>
   );
